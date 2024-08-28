90c0a09baea32141e882e178467e1348e02277b2
Samir <samirb80440@gmail.com>
2024-08-28 15:54:15 +0200

    symphony twig

---
+++ grafikart/src/Controller/RecipeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RecipeController extends AbstractController
+{
+
+    #[Route('/recettes', name: 'recipe.index')]
+    public function index(Request $request): Response
+    {
+        return $this->render('recipe/index.html.twig');
+    }
+    
+    
+    
+    
+    #[Route('/recettes/{slug}-{id}', name: 'recipe.show',requirements: ['id' => '\d+','slug' => '[a-z0-9-]+'])]
+    public function show(Request $request,string $slug,int $id): Response
+    {
+        return $this->render('recipe/show.html.twig', [
+            'slug' => $slug,
+            'id' => $id
+        ]);
+
+    }
+}
+
+
+
+
+
+
