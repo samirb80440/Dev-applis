d664197f08db00503e875396cdfa18f1b31bdaaa
Samir <samirb80440@gmail.com>
2024-08-30 08:31:23 +0200

    orm finie

--- grafikart/src/Controller/RecipeController.php
+++ grafikart/src/Controller/RecipeController.php
@@ -6,25 +6,36 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Recipe;
 
 class RecipeController extends AbstractController
 {
 
     #[Route('/recettes', name: 'recipe.index')]
-    public function index(Request $request): Response
+    public function index(Request $request, RecipeRepository $repository, EntityManagerInterface $em): Response
     {
-        return $this->render('recipe/index.html.twig');
+       $recipes = $repository->findWithDurationLowerThan(20);
+       
+       return $this->render('recipe/index.html.twig', [
+       'recipes' => $recipes
+    ]);
     }
     
     
     
     
     #[Route('/recettes/{slug}-{id}', name: 'recipe.show',requirements: ['id' => '\d+','slug' => '[a-z0-9-]+'])]
-    public function show(Request $request,string $slug,int $id): Response
+    public function show(Request $request,string $slug,int $id, RecipeRepository $repository): Response
     {
+        $recipe = $repository->find($id);
+        if ($recipe->getSlug() !== $slug){
+            return $this->redirectToRoute('recipe.show',['slug' => $recipe->getSlug(), 'id' => $recipe->getId()]);
+        }
+
         return $this->render('recipe/show.html.twig', [
-            'slug' => $slug,
-            'id' => $id
+            'recipe' => $recipe
         ]);
 
     }
