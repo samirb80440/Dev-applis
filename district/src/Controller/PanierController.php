c18eb1982a3a83f0d8b056622e3174ec7a9d1965
Samir <samirb80440@gmail.com>
2024-09-16 10:44:26 +0200

    panier fait

--- district/src/Controller/PanierController.php
+++ district/src/Controller/PanierController.php
@@ -5,23 +5,100 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Entity\Plat;
+use App\Repository\PlatRepository;
 
 class PanierController extends AbstractController
 {
     #[Route('/panier', name: 'app_panier')]
-    public function index(): Response
-    {
-        return $this->render('panier/index.html.twig', [
-            'controller_name' => 'PanierController',
-        ]);
+    public function index(SessionInterface $session,PlatRepository $platrepo): Response
+    {    
+        $panier= $session->get('panier',[]);
+
+        $dataPanier = [];
+        $total =0;
+
+        foreach($panier as $id =>$quantite){
+            $plat = $platrepo->find($id);
+            $dataPanier[] = [
+             "plat"=> $plat,
+             "quantite" => $quantite       
+    
+            ];
+              $total += $plat->getPrix()*$quantite;
+            
+            }
+            
+            count($dataPanier);
+        
+        return $this->render('panier/index.html.twig', compact("dataPanier","total"));
     }
   
-    #[Route('/panier', name: 'app_panier')]
-    public function Addplats(): Response
+    #[Route('/panier/ajout/{id}', name: 'app_ajout_panier', requirements: ['id' => '\d+'])]
+    public function Ajoutplats(SessionInterface $session,Plat $plat): Response
     {
-        return $this->render('panier/index.html.twig', [
-            'controller_name' => 'PanierController',
-        ]);
+        $panier = $session->get('panier', []);
+        $id = $plat->getId();
+
+        if (!empty($panier[$id])){
+            $panier[$id]++;
+        } else{
+            $panier[$id] = 1;
+        }
+
+        $session->set('panier', $panier);
+
+        return $this->redirectToRoute('app_panier');
     }
+
+    #[Route('/panier/enlever/{id}', name: 'app_enlever_panier',requirements:['id' => '\d+'])]
+    public function Enleverplats(SessionInterface $session,Plat $plat): Response
+    {
+    $panier =$session->get('panier',[]);
+    $id = $plat->getId();
+
+    if(!empty($panier[$id])){
+        if($panier[$id]>1){
+            $panier[$id]--;
+            }else{
+                unset($panier[$id]);
+            }}
+            
+
+            $session->set('panier',$panier);
+            return $this->redirectToRoute('app_panier');
+    }
+
+
+    #[Route('/panier/supprimer/{id}', name: 'app_supprimer_panier',requirements: ['id' => '\d+'])]
+    public function Supprimerplats(SessionInterface $session, Plat $plat): Response
+    {
+        $panier = $session->get('panier',[]);
+        $id = $plat->getId();
+
+
+        if (!empty($panier[$id])){
+            unset($panier[$id]);}
+
+        $session->set('panier', $panier);
+
+
+        return $this->redirectToRoute('app_panier');
+    }
+
+    #[Route('/panier/supprimer/all', name: 'app_supprimer_panier_all')]
+    public function Supprimerpanier(SessionInterface $session): Response
+    {
+       $session->set('panier',[]);
+
+       return $this->redirectToRoute('app_panier');
+
+
+
+    }
+
+
+
 
 }
