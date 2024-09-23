27ebaa856f54fb4b5e49063e99aacb2653ebdaa1
Samir <samirb80440@gmail.com>
2024-09-23 11:45:54 +0200

    CSS fini sauf Accueil a régler

--- district/src/Form/RegistrationFormType.php
+++ district/src/Form/RegistrationFormType.php
@@ -67,7 +67,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('Password', PasswordType::class, [
+            ->add('plainPassword', PasswordType::class, [
                                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
