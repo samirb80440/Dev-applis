45a029d3d27744883175667c57f35d85c00f24c4
Samir <samirb80440@gmail.com>
2024-10-07 15:09:54 +0200

    formulaire avec contraintes

--- district/src/Form/CommandeType.php
+++ district/src/Form/CommandeType.php
@@ -6,16 +6,10 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Event\PostSubmitEvent;
-use Symfony\Component\Form\Event\PreSubmitEvent;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -25,55 +19,121 @@ class CommandeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('nom',TextType::class,[
-            'attr' => [
-                'class' => 'col-3 form-control'
-            ]
-        ])
-        ->add('prenom',TextType::class,[
-            'attr' => [
-                'class' => 'col-3 form-control'
-            ]
-        ])
-        ->add('telephone',TextType::class,[
-            'attr' => [
-                'class' => 'col-3 form-control'
-            ]
-        ])
-        ->add('adresse',TextType::class,[
-            'attr' => [
-                'class' => 'col-3 form-control'
-            ]
-        ])
-        ->add('cp',TextType::class,[
-            'attr' => [
-                'class' => 'col-3 form-control'
-            ]
-        ])
-        ->add('ville',TextType::class,[
-            'attr' => [
-                'class' => 'col-3 form-control'
-            ]
-        ])
-        ->add('agreeTerms', CheckboxType::class, [
-           'mapped' => false,
-            'constraints' => [
-                new IsTrue([
-                    'message' => 'You should agree to our terms.',
-                ]),
-            ],
-        ])
-        ->add('save', SubmitType::class, [
-            'label' => 'Commander',
-            'attr' => [
-                'class' => 'btn color-B09595 rounded-pill '
-            ],
-            'row_attr' => [
-                'class' => 'd-flex justify-content-end'
-            ]
-        ])
-    ;
-}
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'class' => 'col-3 form-control'
+                ],
+                'contraintes' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre nom.',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Votre nom doit contenir au moins {{ limit }} caractères.',
+                        'max' => 50,
+                        'maxMessage' => 'Votre nom ne doit pas contenir plus de {{ limit }} caractères.',
+                    ]),
+                ],
+            ])
+            ->add('prenom', TextType::class, [
+                'attr' => [
+                    'class' => 'col-3 form-control'
+                ],
+                'contraintes' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre prénom.',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Votre prénom doit contenir au moins {{ limit }} caractères.',
+                        'max' => 50,
+                        'maxMessage' => 'Votre prénom ne doit pas contenir plus de {{ limit }} caractères.',
+                    ]),
+                ],
+            ])
+            ->add('telephone', TextType::class, [
+                'attr' => [
+                    'class' => 'col-3 form-control'
+                ],
+                'contraintes' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre numéro de téléphone.',
+                    ]),
+                    new Length([
+                        'min' => 10,
+                        'minMessage' => 'Votre numéro de téléphone doit contenir au moins {{ limit }} caractères.',
+                        'max' => 15,
+                        'maxMessage' => 'Votre numéro de téléphone ne doit pas contenir plus de {{ limit }} caractères.',
+                    ]),
+                ],
+            ])
+            ->add('adresse', TextType::class, [
+                'attr' => [
+                    'class' => 'col-3 form-control'
+                ],
+                'contraintes' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre adresse.',
+                    ]),
+                    new Length([
+                        'min' => 10,
+                        'minMessage' => 'Votre adresse doit contenir au moins {{ limit }} caractères.',
+                        'max' => 100,
+                        'maxMessage' => 'Votre adresse ne doit pas contenir plus de {{ limit }} caractères.',
+                    ]),
+                ],
+            ])
+            ->add('cp', TextType::class, [
+                'attr' => [
+                    'class' => 'col-3 form-control'
+                ],
+                'contraintes' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre code postal.',
+                    ]),
+                    new Length([
+                        'min' => 5,
+                        'minMessage' => 'Votre code postal doit contenir au moins {{ limit }} caractères.',
+                        'max' => 10,
+                        'maxMessage' => 'Votre code postal ne doit pas contenir plus de {{ limit }} caractères.',
+                    ]),
+                ],
+            ])
+            ->add('ville', TextType::class, [
+                'attr' => [
+                    'class' => 'col-3 form-control'
+                ],
+                'contraintes' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre ville.',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Votre ville doit contenir au moins {{ limit }} caractères.',
+                        'max' => 50,
+                        'maxMessage' => 'Votre ville ne doit pas contenir plus de {{ limit }} caractères.',
+                    ]),
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'contraintes' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter nos termes.',
+                    ]),
+                ],
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Commander',
+                'attr' => [
+                    'class' => 'btn color-B09595 rounded-pill '
+                ],
+                'row_attr' => [
+                    'class' => 'd-flex justify-content-end'
+                ]
+            ])
+        ;
+    }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
--- district/src/Form/ContactFormType.php
+++ district/src/Form/ContactFormType.php
@@ -10,42 +10,98 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Regex;
+
 
 class ContactFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Nom',TextType::class,[
+            ->add('Nom', TextType::class, [
                 'attr' => [
                     'class' => 'col-3 form-control'
-                ]
+                ],
+                'contraintes' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre nom.',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Votre nom doit contenir au moins {{ limit }} caractères.',
+                        'max' => 50,
+                        'maxMessage' => 'Votre nom ne doit pas contenir plus de {{ limit }} caractères.',
+                    ]),
+                ],
             ])
-            ->add('Prenom',TextType::class,[
+            ->add('Prenom', TextType::class, [
                 'attr' => [
                     'class' => 'col-3 form-control'
-                ]
+                ],
+                'contraintes' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre prénom.',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Votre prénom doit contenir au moins {{ limit }} caractères.',
+                        'max' => 50,
+                        'maxMessage' => 'Votre prénom ne doit pas contenir plus de {{ limit }} caractères.',
+                    ]),
+                ],
             ])
-            ->add('Email',EmailType::class,[
+            ->add('Email', EmailType::class, [
                 'attr' => [
-                    'class' => ' col-3 form-control texte'
-                ]
+                    'class' => 'col-3 form-control texte'
+                ],
+                'contraintes' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre adresse e-mail.',
+                    ]),
+                    new Email([
+                        'message' => 'Veuillez entrer une adresse e-mail valide.',
+                    ]),
+                ],
             ])
-            ->add('Telephone',TextType::class,[
+            ->add('Telephone', TextType::class, [
                 'attr' => [
                     'class' => 'col-3 form-control'
-                ]
+                ],
+                'contraintes' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre numéro de téléphone.',
+                    ]),
+                    new Regex([
+                        'pattern' => '/^(0|0033)[1-9]([-. ]?\d{2}){4}$/',
+                        'message' => 'Veuillez entrer un numéro de téléphone valide.',
+                    ]),
+                ],
             ])
-            ->add('Demande',TextareaType::class,[
-                'label'=> 'Votre Demande',
-                    'attr' => [
-                        'class' => 'col-3 form-control'
-                    ]
+            ->add('Demande', TextareaType::class, [
+                'label' => 'Votre Demande',
+                'attr' => [
+                    'class' => 'col-3 form-control'
+                ],
+                'contraintes' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre demande.',
+                    ]),
+                    new Length([
+                        'min' => 10,
+                        'minMessage' => 'Votre demande doit contenir au moins {{ limit }} caractères.',
+                        'max' => 500,
+                        'maxMessage' => 'Votre demande ne doit pas contenir plus de {{ limit }} caractères.',
+                    ]),
+                ],
             ])
             ->add('Save', SubmitType::class, [
                 'label' => 'Envoyer',
                 'attr' => [
-                    'class' => 'btn  color-B09595 rounded-pill '
+                    'class' => 'btn color-B09595 rounded-pill '
                 ],
                 'row_attr' => [
                     'class' => 'd-flex justify-content-end'
